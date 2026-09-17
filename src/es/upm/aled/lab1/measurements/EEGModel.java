3f814f716c85f35f0233bf873cd262bc4d23a736
danie <danie@DANI-ASEN06>
2026-09-17 20:24:56 +0200

    Empezado saveFile

--- src/es/upm/aled/lab1/measurements/EEGModel.java
+++ src/es/upm/aled/lab1/measurements/EEGModel.java
@@ -131,7 +131,9 @@ public class EEGModel {
 	 */
 	public void saveFile(String fileName) throws IOException {
 		// TODO
-		
+		File f = new File(fileName);
+		FileOutputStream fis = new FileOutputStream(f);
+		PrintStream fid = new PrintStream(fis);
 	}
 
 	/**
